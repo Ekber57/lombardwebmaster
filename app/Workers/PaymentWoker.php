2b182ea2852a9c521d4f18e743c63f0a369bf253
Ekber Quliyev <ekberquliyev57@gmail.com>
2024-02-23 16:05:17 +0400

    Kredit odenisine tranzaksiya elave edildi

--- app/Workers/PaymentWoker.php
+++ app/Workers/PaymentWoker.php
@@ -2,51 +2,59 @@
 
 namespace App\Workers;
 
-use App\DTOS\PaymentDTO;
-use App\Models\Payment;
 use Carbon\Carbon;
+use App\Models\Payment;
+use App\DTOS\PaymentDTO;
+use Illuminate\Support\Facades\DB;
 
 class PaymentWoker
 {
     public function pay(PaymentDTO $paymentDTO)
     {
-        $amount = $paymentDTO->amount;
-        $paymentDTO->last_remainder = $paymentDTO->credit->remainder;
-        $paymentDTO->credit = $paymentDTO->credit;
-        if ($paymentDTO->amount  >= $paymentDTO->credit->percentage_amount) {
-            //! Bir ayliq odenis et
-            // echo "odenis " . $paymentDTO->amount . "<br>";
-            $paymentDTO->amount = round($paymentDTO->amount -  $paymentDTO->credit->percentage_amount, 2);
-        
-            if ($paymentDTO->amount > 0) {
-                $paymentDTO->credit->remainder = round($paymentDTO->credit->remainder - $paymentDTO->amount, 2);
-                // echo "Remainder azaldi".$paymentDTO->credit->remainder;;
+        DB::beginTransaction();
+        try {
+            $amount = $paymentDTO->amount;
+            $paymentDTO->last_remainder = $paymentDTO->credit->remainder;
+            $paymentDTO->credit = $paymentDTO->credit;
+            if ($paymentDTO->amount  >= $paymentDTO->credit->percentage_amount) {
+                //! Bir ayliq odenis et
+                // echo "odenis " . $paymentDTO->amount . "<br>";
+                $paymentDTO->amount = round($paymentDTO->amount -  $paymentDTO->credit->percentage_amount, 2);
+            
+                if ($paymentDTO->amount > 0) {
+                    $paymentDTO->credit->remainder = round($paymentDTO->credit->remainder - $paymentDTO->amount, 2);
+                    // echo "Remainder azaldi".$paymentDTO->credit->remainder;;
+                }
+                
+                $monthly_percentage = ($paymentDTO->credit->percentage / (12    * 100));
+                $paymentDTO->credit->percentage_amount = round(($paymentDTO->credit->remainder * $monthly_percentage), 2);
+                $paymentDTO->credit->base_debt = $paymentDTO->credit->annuted - $paymentDTO->credit->percentage_amount;
+                $paymentDTO->credit->payment_amount  =  round($paymentDTO->credit->percentage_amount + $paymentDTO->credit->base_debt, 2);
+                $paymentDTO->credit->next_payment_date = (Carbon::parse($paymentDTO->credit->next_payment_date)->addMonth());
+                $paymentDTO->payment = $this->addPayment($paymentDTO,$amount);
+            } 
+            
+            else {
+                $date = Carbon::parse($paymentDTO->credit->next_payment_date);
+                $newDate = $date->addMonths(1);
+                $paymentDTO->credit->next_payment_date = $newDate;
+                $paymentDTO->payment = $this->addPayment($paymentDTO,$amount);
             }
-            
-            $monthly_percentage = ($paymentDTO->credit->percentage / (12    * 100));
-            $paymentDTO->credit->percentage_amount = round(($paymentDTO->credit->remainder * $monthly_percentage), 2);
-            $paymentDTO->credit->base_debt = $paymentDTO->credit->annuted - $paymentDTO->credit->percentage_amount;
-            $paymentDTO->credit->payment_amount  =  round($paymentDTO->credit->percentage_amount + $paymentDTO->credit->base_debt, 2);
-            $paymentDTO->credit->next_payment_date = (Carbon::parse($paymentDTO->credit->next_payment_date)->addMonth());
-            $paymentDTO->payment = $this->addPayment($paymentDTO,$amount);
-        } 
-        
-        else {
-            $date = Carbon::parse($paymentDTO->credit->next_payment_date);
-            $newDate = $date->addMonths(1);
-            $paymentDTO->credit->next_payment_date = $newDate;
-            $paymentDTO->payment = $this->addPayment($paymentDTO,$amount);
+    
+            if($paymentDTO->credit->remainder == 0) {
+                $paymentDTO->credit->base_debt = 0;
+                $paymentDTO->credit->percentage_amount = 0;
+                $paymentDTO->credit->status = 1;
+                
+            }
+            $paymentDTO->credit->last_payment_date = Carbon::today();
+            $paymentDTO->credit->save();
+            DB::commit();    
+            //code...
+        } catch (\Throwable $th) {
+            DB::rollBack();
         }
-
-        if($paymentDTO->credit->remainder == 0) {
-            $paymentDTO->credit->base_debt = 0;
-            $paymentDTO->credit->percentage_amount = 0;
-            $paymentDTO->credit->status = 1;
-            
-        }
-        $paymentDTO->credit->last_payment_date = Carbon::today();
-        $paymentDTO->credit->save();
-        
+       
     }
 
 
